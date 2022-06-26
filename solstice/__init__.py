9e3a75e47cd0a203841894c07a9bc960c4470c15
Charles Jones <cj1917@ic.ac.uk>
2022-06-26 14:08:20 +0000

    remove logger

--- solstice/__init__.py
+++ solstice/__init__.py
@@ -1,7 +1,6 @@
 """Solstice, a library for creating and scaling experiments in JAX."""
 
 from solstice.experiment import ClassificationExperiment, Experiment
-from solstice.logger import Logger, TensorBoardLogger, WandbLogger
 from solstice.metrics import ClassificationMetrics, Metrics
 from solstice import compat as compat
 
@@ -9,8 +8,5 @@ __all__ = (
     "ClassificationExperiment",
     "ClassificationMetrics",
     "Experiment",
-    "Logger",
     "Metrics",
-    "TensorBoardLogger",
-    "WandbLogger",
 )
