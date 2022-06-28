86e4dab677510eb6620948d304312735eff5e352
Charles Jones <cj1917@ic.ac.uk>
2022-06-28 09:42:40 +0000

    fix optimizer docstring

--- solstice/compat/optimizer.py
+++ solstice/compat/optimizer.py
@@ -3,10 +3,12 @@ class for wrapping Optax optimizers, and an example implementation of SGD."""
 
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Tuple
+from typing import TYPE_CHECKING, Tuple
 import dataclasses
 import equinox as eqx
-import optax
+
+if TYPE_CHECKING:
+    import optax
 
 
 class Optimizer(eqx.Module, ABC):
@@ -39,8 +41,8 @@ class OptaxOptimizer(Optimizer):
     ) -> None:
         """
         Args:
-            - optimizer (optax.GradientTransformation): Optax optimizer to wrap.
-            - params (optax.Params): Model params for initialising optimizer.
+            optimizer (optax.GradientTransformation): Optax optimizer to wrap.
+            params (optax.Params): Model params for initialising optimizer.
         """
         self.update_fn = optimizer.update
         self.opt_state = optimizer.init(params)
@@ -50,21 +52,23 @@ class OptaxOptimizer(Optimizer):
     ) -> Tuple[optax.Updates, OptaxOptimizer]:
         """
         Args:
-            - grads (optax.Params): PyTree of gradients.
-            - params (optax.Params | None, optional): PyTree of current parameters
+            grads (optax.Params): PyTree of gradients.
+            params (optax.Params | None, optional): PyTree of current parameters
                 with same shape as grads. Only needed for optimizers which use the
                 current params to determine the updates. Defaults to None.
 
         Returns:
-            - Tuple[optax.Updates, OptaxOptimizer]: Tuple of updates with the same shape
+            Tuple[optax.Updates, OptaxOptimizer]: Tuple of updates with the same shape
                 as the grads and params, and an updated Optimizer object with the new
                 state.
         """
 
-        updates, new_state = self.update_fn(self.opt_state, grads, params)
+        updates, new_state = self.update_fn(grads, self.opt_state, params)
         return updates, dataclasses.replace(self, opt_state=new_state)
 
 
+# this is just an example of how you could use Solstice to make your own optimizer.
+# Realistically, using Optax with the OptaxOptimizer wrapper is a more common case.
 class SGDOptimizer(Optimizer):
     """Solstice implementation of Stochastic Gradient Descent optimizer."""
 
