c980202ef9fe4adca14f1dd8678e993948e24a57
Charles Jones <cj1917@ic.ac.uk>
2022-06-28 10:18:31 +0000

    update module docstring

--- solstice/compat/__init__.py
+++ solstice/compat/__init__.py
@@ -1,6 +1,6 @@
-"""solstice.compat is a set of optional compatibility layers for giving common
+"""Solstice.compat is a set of optional compatibility layers for giving common
 interfaces to popular JAX libraries. This allows you to write training code in
-Solstice that is not dependent on any neural network/optimisation/dataset libraries."""
+Pure JAX+Equinox without depending on neural network or optimization libraries."""
 
 from solstice.compat.optimizer import Optimizer, OptaxOptimizer, SGDOptimizer
 
