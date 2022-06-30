aa55c3f28142cd47f986184720a239e04f78ce50
Charles Jones <cj1917@ic.ac.uk>
2022-06-30 13:21:53 +0000

    create trainer file

---
+++ solstice/trainer.py
@@ -0,0 +1,4 @@
+"""Training loops are usually boilerplate code that has little to do with your research.
+We provide a trainer class which integrates with a simple and flexible callback system.
+Any `solstice.Experiment` can be passed to the trainer, but you can always write your
+own training loops if necessary."""
